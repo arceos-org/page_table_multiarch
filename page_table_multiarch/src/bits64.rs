feb4aa3fa4589b5738a3ff40ff875870e4ce2614
aarkegz <aarkegz@gmail.com>
2024-08-27 00:38:53 +0800

    formatted

--- page_table_multiarch/src/bits64.rs
+++ page_table_multiarch/src/bits64.rs
@@ -3,7 +3,7 @@ extern crate alloc;
 use crate::{GenericPTE, PagingHandler, PagingMetaData};
 use crate::{MappingFlags, PageSize, PagingError, PagingResult, TlbFlush, TlbFlushAll};
 use core::marker::PhantomData;
-use memory_addr::{PhysAddr, PAGE_SIZE_4K, MemoryAddr};
+use memory_addr::{MemoryAddr, PhysAddr, PAGE_SIZE_4K};
 
 const ENTRY_COUNT: usize = 512;
 
