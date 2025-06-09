61b110f82f333811982bd6e7a52288e40ea01acc
mingzi <mingzi0147@gmail.com>
2025-06-09 15:50:15 +0800

    fix: correct TLB invalidation address calculation for aarch64
    - Shift and mask vaddr before TLB invalidation
    
    - Maintain same behavior for VMID invalidation

--- page_table_multiarch/src/arch/aarch64.rs
+++ page_table_multiarch/src/arch/aarch64.rs
@@ -24,7 +24,7 @@ impl PagingMetaData for A64PagingMetaData {
         unsafe {
             if let Some(vaddr) = vaddr {
                 // TLB Invalidate by VA, All ASID, EL1, Inner Shareable
-                asm!("tlbi vaae1is, {}; dsb sy; isb", in(reg) vaddr.as_usize())
+                asm!("tlbi vaae1is, {}; dsb sy; isb", in(reg) ((vaddr.as_usize() >> 12) & 0xFFFF_FFFF_FFFF))
             } else {
                 // TLB Invalidate by VMID, All at stage 1, EL1
                 asm!("tlbi vmalle1; dsb sy; isb")
