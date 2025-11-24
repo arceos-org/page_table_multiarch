93237a9d465a36c6fdebd42b96261cd76fc21805
朝倉水希 <asakuramizu111@gmail.com>
2025-11-24 23:03:53 +0800

    fix: use `--cfg docsrs` for document (#27)

--- page_table_entry/src/arch/mod.rs
+++ page_table_entry/src/arch/mod.rs
@@ -1,11 +1,15 @@
-#[cfg(any(target_arch = "x86_64", doc))]
+#[cfg(any(target_arch = "x86_64", doc, docsrs))]
+#[cfg_attr(doc, doc(cfg(target_arch = "x86_64")))]
 pub mod x86_64;
 
-#[cfg(any(target_arch = "riscv32", target_arch = "riscv64", doc))]
+#[cfg(any(target_arch = "riscv32", target_arch = "riscv64", doc, docsrs))]
+#[cfg_attr(doc, doc(cfg(any(target_arch = "riscv32", target_arch = "riscv64"))))]
 pub mod riscv;
 
-#[cfg(any(target_arch = "aarch64", doc))]
+#[cfg(any(target_arch = "aarch64", doc, docsrs))]
+#[cfg_attr(doc, doc(cfg(target_arch = "aarch64")))]
 pub mod aarch64;
 
-#[cfg(any(target_arch = "loongarch64", doc))]
+#[cfg(any(target_arch = "loongarch64", doc, docsrs))]
+#[cfg_attr(doc, doc(cfg(target_arch = "loongarch64")))]
 pub mod loongarch64;
