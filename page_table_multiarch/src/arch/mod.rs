c0fecc3509d5d16f159f8b6862effed0f394bf79
ctrlshiftm129 <127505439+ctrlshiftm129@users.noreply.github.com>
2025-11-28 11:22:39 +0800

    fix: Can't build documentation locally

--- page_table_multiarch/src/arch/mod.rs
+++ page_table_multiarch/src/arch/mod.rs
@@ -1,15 +1,15 @@
-#[cfg(any(target_arch = "x86_64", doc, docsrs))]
-#[cfg_attr(doc, doc(cfg(target_arch = "x86_64")))]
+#[cfg(any(target_arch = "x86_64", docsrs))] 
+#[cfg_attr(docsrs, doc(cfg(target_arch = "x86_64")))]
 pub mod x86_64;
 
-#[cfg(any(target_arch = "riscv32", target_arch = "riscv64", doc, docsrs))]
-#[cfg_attr(doc, doc(cfg(any(target_arch = "riscv32", target_arch = "riscv64"))))]
+#[cfg(any(target_arch = "riscv32", target_arch = "riscv64", docsrs))]
+#[cfg_attr(docsrs, doc(cfg(any(target_arch = "riscv32", target_arch = "riscv64"))))]
 pub mod riscv;
 
-#[cfg(any(target_arch = "aarch64", doc, docsrs))]
-#[cfg_attr(doc, doc(cfg(target_arch = "aarch64")))]
+#[cfg(any(target_arch = "aarch64", docsrs))]
+#[cfg_attr(docsrs, doc(cfg(target_arch = "aarch64")))]
 pub mod aarch64;
 
-#[cfg(any(target_arch = "loongarch64", doc, docsrs))]
-#[cfg_attr(doc, doc(cfg(target_arch = "loongarch64")))]
+#[cfg(any(target_arch = "loongarch64", docsrs))]
+#[cfg_attr(docsrs, doc(cfg(target_arch = "loongarch64")))]
 pub mod loongarch64;
